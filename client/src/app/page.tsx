53ac4d700fd3669799d6826b7a74c882a3470702
masonnguyen1243 <meowjcisgod@gmail.com>
2025-11-04 15:20:27 +0700

    feat: Responsive home design

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,3 +1,13 @@
+import ProductList from "@/components/ProductList";
+import Image from "next/image";
+
 export default function Home() {
-  return <div className="text-red-500">Home page</div>;
+  return (
+    <div className="">
+      <div className="relative aspect-3/1 mb-12">
+        <Image src={"/featured.png"} alt="Featured Product" fill />
+      </div>
+      <ProductList />
+    </div>
+  );
 }
---
+++ client/src/components/ProductCard.tsx
@@ -0,0 +1,104 @@
+"use client";
+
+import { ProductType } from "@/types";
+import { ShoppingBag } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+
+const ProductCard = ({ product }: { product: ProductType }) => {
+  const [productType, setProductType] = useState({
+    size: product.sizes[0],
+    color: product.colors[0],
+  });
+
+  const handleProductType = ({
+    type,
+    value,
+  }: {
+    type: "size" | "color";
+    value: string;
+  }) => {
+    setProductType((prev) => ({ ...prev, [type]: value }));
+  };
+
+  console.log("type", productType);
+
+  return (
+    <div className="shadow-lg rounded-lg overflow-hidden">
+      {/* Image */}
+      <Link href={`/products/${product.id}`}>
+        <div className="relative aspect-2/3 hover:scale-105 transition-all duration-300">
+          <Image
+            src={product.images[productType.color]}
+            alt={product.name}
+            fill
+            className="object-cover"
+          />
+        </div>
+      </Link>
+
+      {/* Product details */}
+      <div className="flex flex-col gap-4 p-4">
+        <h1 className="font-medium">{product.name}</h1>
+        <p className="text-sm text-gray-500">{product.shortDescription}</p>
+        {/* Product types */}
+        <div className="flex items-center gap-4 text-xs">
+          {/* Sizes */}
+          <div className="flex flex-col gap-1">
+            <span className="text-gray-500">Size</span>
+            <select
+              name="size"
+              id="size"
+              onChange={(e) =>
+                handleProductType({ type: "size", value: e.target.value })
+              }
+              className="ring ring-gray-300 rounded-md px-2 py-1 "
+            >
+              {product.sizes.map((size) => (
+                <option key={size} value={size}>
+                  {size.toUpperCase()}
+                </option>
+              ))}
+            </select>
+          </div>
+          {/* Colors */}
+          <div className="flex flex-col gap-1">
+            <span className="text-gray-500">Colors</span>
+            <div className="flex items-center gap-2">
+              {product.colors.map((color) => (
+                <div
+                  className={`cursor-pointer border rounded-full p-[1.2px] ${
+                    productType.color === color
+                      ? "border-gray-400"
+                      : "border-gray-200"
+                  }`}
+                  key={color}
+                  onClick={() =>
+                    handleProductType({ type: "color", value: color })
+                  }
+                >
+                  <div
+                    className="w-3.5 h-3.5 rounded-full"
+                    style={{ backgroundColor: color }}
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+
+        {/* Price and  add to cart */}
+        <div className="flex items-center justify-between">
+          <p className="font-medium">${product.price.toFixed(2)}</p>
+          <button className="ring-1 ring-gray-200 shadow-lg rounded-md px-2 py-1 text-sm cursor-pointer hover:text-white hover:bg-black transition-all duration-300 flex items-center gap-2">
+            <ShoppingBag className="w-4 h-4" />
+            Add to cart
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
