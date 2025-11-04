b678a44bc259b33a1fb45cfc760537316281cde6
masonnguyen1243 <meowjcisgod@gmail.com>
2025-11-04 15:42:38 +0700

    feat: Product list page design and filter

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -13,7 +13,7 @@ export default async function Home({
       <div className="relative aspect-3/1 mb-12">
         <Image src={"/featured.png"} alt="Featured Product" fill />
       </div>
-      <ProductList category={category} />
+      <ProductList category={category} params="homepage" />
     </div>
   );
 }
