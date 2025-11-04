499adffb2f77ef51070d75a0e694e90710b60099
masonnguyen1243 <meowjcisgod@gmail.com>
2025-11-04 20:08:32 +0700

    feat: Payment form with react-hook-form & zod

--- client/src/components/PaymentForm.tsx
+++ client/src/components/PaymentForm.tsx
@@ -1,5 +1,142 @@
+import { PaymentFormInputs, paymentFormSchema } from "@/types";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { ShoppingCart } from "lucide-react";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+
 const PaymentForm = () => {
-  return <div>PaymentForm</div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<PaymentFormInputs>({
+    resolver: zodResolver(paymentFormSchema),
+  });
+
+  const router = useRouter();
+
+  const handlePaymentForm: SubmitHandler<PaymentFormInputs> = (data) => {
+    router.push("/cart?step=3", { scroll: false });
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit(handlePaymentForm)}
+      className="flex flex-col gap-4"
+    >
+      {/* Name on card */}
+      <div className="flex flex-col gap-1">
+        <label
+          htmlFor="cardHolder"
+          className="text-xs text-gray-500 font-medium"
+        >
+          Name on card
+        </label>
+        <input
+          type="text"
+          id="cardHolder"
+          placeholder="John Doe"
+          {...register("cardHolder")}
+          className="border-b border-gray-200 py-2 outline-none text-sm"
+        />
+        {errors.cardHolder && (
+          <p className="text-xs text-red-500">{errors.cardHolder.message}</p>
+        )}
+      </div>
+
+      {/* Card number */}
+      <div className="flex flex-col gap-1">
+        <label
+          htmlFor="cardNumber"
+          className="text-xs text-gray-500 font-medium"
+        >
+          Card Number
+        </label>
+        <input
+          type="text"
+          id="cardNumber"
+          placeholder="1234 5678 9012 3456"
+          {...register("cardNumber")}
+          className="border-b border-gray-200 py-2 outline-none text-sm"
+        />
+        {errors.cardNumber && (
+          <p className="text-xs text-red-500">{errors.cardNumber.message}</p>
+        )}
+      </div>
+
+      {/* Expiration Date */}
+      <div className="flex flex-col gap-1">
+        <label
+          htmlFor="expirationDate"
+          className="text-xs text-gray-500 font-medium"
+        >
+          Expiration Date
+        </label>
+        <input
+          type="text"
+          id="expirationDate"
+          placeholder="04/27"
+          {...register("expirationDate")}
+          className="border-b border-gray-200 py-2 outline-none text-sm"
+        />
+        {errors.expirationDate && (
+          <p className="text-xs text-red-500">
+            {errors.expirationDate.message}
+          </p>
+        )}
+      </div>
+
+      {/* CVV */}
+      <div className="flex flex-col gap-1">
+        <label htmlFor="cvv" className="text-xs text-gray-500 font-medium">
+          CVV
+        </label>
+        <input
+          type="text"
+          id="cvv"
+          placeholder="678"
+          {...register("cvv")}
+          className="border-b border-gray-200 py-2 outline-none text-sm"
+        />
+        {errors.cvv && (
+          <p className="text-xs text-red-500">{errors.cvv.message}</p>
+        )}
+      </div>
+
+      <div className="flex justify-start gap-2 mt-4">
+        <Image
+          src={"/klarna.png"}
+          alt="klarna"
+          width={50}
+          height={25}
+          className="rounded-md"
+        />
+        <Image
+          src={"/cards.png"}
+          alt="cards"
+          width={50}
+          height={25}
+          className="rounded-md"
+        />
+        <Image
+          src={"/stripe.png"}
+          alt="stripe"
+          width={50}
+          height={25}
+          className="rounded-md"
+        />
+      </div>
+
+      <button
+        type="submit"
+        className="w-full bg-gray-800 text-white p-2 rounded-lg cursor-pointer flex items-center justify-center gap-2 hover:bg-gray-900 transition-all duration-300"
+      >
+        Checkout
+        <ShoppingCart className="w-3 h-3" />
+      </button>
+    </form>
+  );
 };
 
 export default PaymentForm;
--- client/src/types.ts
+++ client/src/types.ts
@@ -1,5 +1,6 @@
 import { z } from "zod";
 
+// Product Type
 export type ProductType = {
   id: string | number;
   name: string;
@@ -14,6 +15,7 @@ export type ProductType = {
 
 export type ProductsType = ProductType[];
 
+// Cart Item Type
 export type CartItemType = ProductType & {
   quantity: number;
   selectedSize: string;
@@ -22,6 +24,7 @@ export type CartItemType = ProductType & {
 
 export type CartItemsType = CartItemType[];
 
+// Shipping Form Schema and Type
 export const shippingFormSchema = z.object({
   name: z.string().min(1, "Name is required"),
   email: z.email().min(1, "Email is required"),
@@ -35,3 +38,21 @@ export const shippingFormSchema = z.object({
 });
 
 export type ShippingFormInputs = z.infer<typeof shippingFormSchema>;
+
+// Payment Form Schema and Type
+export const paymentFormSchema = z.object({
+  cardHolder: z.string().min(1, "Card holder is required"),
+  cardNumber: z
+    .string()
+    .min(16, "Card number must be 16 digits")
+    .max(16, "Card number must be 16 digits"),
+  expirationDate: z
+    .string()
+    .regex(
+      /^(0[1-9]|1[0-2])\/?([0-9]{4}|[0-9]{2})$/,
+      "Expiration date must be in MM/YY format"
+    ),
+  cvv: z.string().min(3, "CVV is required").max(3, "CVV must be 3 digits"),
+});
+
+export type PaymentFormInputs = z.infer<typeof paymentFormSchema>;
