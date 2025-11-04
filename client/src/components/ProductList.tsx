270836c45f7d549b4431f3fdb842a9d3a6ee47ab
masonnguyen1243 <meowjcisgod@gmail.com>
2025-11-04 15:20:40 +0700

    feat: Responsive home design

--- client/src/components/ProductList.tsx
+++ client/src/components/ProductList.tsx
@@ -1,6 +1,7 @@
 import { ProductsType } from "@/types";
 import Categories from "./Categories";
 import ProductCard from "./ProductCard";
+import Link from "next/link";
 
 //Mock data
 const products: ProductsType = [
@@ -114,7 +115,7 @@ const products: ProductsType = [
   },
 ];
 
-const ProductList = () => {
+const ProductList = ({ category }: { category: string }) => {
   return (
     <div className="w-full">
       <Categories />
@@ -123,6 +124,13 @@ const ProductList = () => {
           <ProductCard key={product.id} product={product} />
         ))}
       </div>
+
+      <Link
+        href={category ? `/products?category=${category}` : "/products"}
+        className="flex justify-end mt-4 underline text-sm text-gray-500"
+      >
+        See all products
+      </Link>
     </div>
   );
 };
